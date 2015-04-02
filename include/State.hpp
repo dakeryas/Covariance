8d688fe6507f33ce38661748e9f305dddaae7023
Dakeryas <mrdakeryas@gmail.com>
2015-04-02 17:41:51 +0200

    The ratios are now part of the State class (the vector will remain empty for FinalState) so UnstableState has no member variables

--- include/State.hpp
+++ include/State.hpp
@@ -4,16 +4,18 @@
 #include <iomanip>
 #include <vector>
 #include <utility>
-#include "Ratio.hpp"
+#include "UniformRatio.hpp"
 
 template <class T> 
 class State{ //node interface
 
 protected:
   std::vector<std::unique_ptr<State<T>>> daughters;
+  std::vector<std::unique_ptr<Ratio>> ratios;//ratios to the daughters
   
 public:
   State()= default;
+  State(std::vector<State<T>*> daughters, std::vector<Ratio*> ratios);//it always takes ownership of the pointers passed, give the ratios to the daughters
   State(std::vector<State<T>*> daughters);//it always takes ownership of the pointers passed
   State(const State<T>& other);
   State(State<T>&& other) = default;
@@ -23,6 +25,9 @@ public:
   virtual unsigned getDimensionOfRealisations() const = 0;//returns the dimension of the first found data stored in the daughters
   virtual T getRealisation(std::mt19937& randomGenerator) = 0;//picks random ratios and returns the resulting spectrum or returns the spectrum for a leaf
   const std::vector<std::unique_ptr<State<T>>>& getDaughters() const;
+  std::vector<std::unique_ptr<State<T>>>& getDaughters();//returns assignable reference to the daughters
+  virtual void addDaughter(State<T>* daughter);
+  virtual void addDaughter(State<T>* daughter, Ratio* ratio);
   virtual std::unique_ptr<State<T>> clone() const = 0;//for the copy constructor that needs polymorphism
   static void print(std::ostream& output, const State<T>& state, unsigned depth);//print state knowing its depth in the root state that contains it
   
@@ -45,7 +50,14 @@ std::ostream& operator<<(std::ostream& output, const State<T>& state){
 }
 
 template <class T>
-State<T>::State(std::vector< State<T>* > daughters):daughters(daughters.begin(),daughters.end()){
+State<T>::State(std::vector<State<T>*> daughters, std::vector<Ratio*> ratios):daughters(daughters.begin(),daughters.end()),ratios(ratios.begin(),ratios.end()){
+  
+}
+
+template <class T>
+State<T>::State(std::vector< State<T>* > daughters):State(daughters,{}){
+  
+  for(const auto& daughter : daughters) ratios.emplace_back(new UniformRatio);
 
 }
 
@@ -54,6 +66,8 @@ State<T>::State(const State<T>& other){//we don't want to move the unique_ptr's 
 
   daughters.reserve(other.daughters.size());
   for(const auto& daughter : other.daughters) this->daughters.emplace_back(daughter->clone());
+  ratios.reserve(other.ratios.size());
+  for(const auto& ratio : other.ratios) this->ratios.emplace_back(ratio->clone());
   
 }
 
@@ -62,6 +76,10 @@ State<T>& State<T>::operator=(const State<T>& other){
   
   daughters.resize(other.daughters.size());
   for(auto it = std::make_pair(daughters.begin(), other.daughters.begin()); it.first != daughters.end() && it.second != other.daughters.end(); ++it.first, ++it.second)
+    *it.first = std::move((*it.second)->clone());
+  
+  ratios.resize(other.ratios.size());
+  for(auto it = std::make_pair(ratios.begin(), other.ratios.begin()); it.first != ratios.end() && it.second != other.ratios.end(); ++it.first, ++it.second)
     *it.first = std::move((*it.second)->clone());
 
 }
@@ -73,4 +91,25 @@ const std::vector<std::unique_ptr<State<T>>>& State<T>::getDaughters() const{
 
 }
 
+template <class T>
+std::vector<std::unique_ptr<State<T>>>& State<T>::getDaughters(){
+  
+  return daughters;
+
+}
+
+template <class T>
+void State<T>::addDaughter(State<T>* daughter, Ratio* ratio){
+  
+  daughters.emplace_back(daughter);
+  ratios.emplace_back(ratio);
+  
+}
+
+template <class T>
+void State<T>::addDaughter(State<T>* daughter){
+  
+  addDaughter(daughter, new UniformRatio);
+  
+}
 #endif
--- include/UnstableState.hpp
+++ include/UnstableState.hpp
@@ -3,61 +3,37 @@
 
 #include "Eigen/Core"
 #include "State.hpp"
-#include "UniformRatio.hpp"
 
 template <class T> 
 class UnstableState: public State<T>{ //node class using the Mersenne Twister generator for the ratios of the edges
 
-  std::vector<std::unique_ptr<Ratio>> ratios;
-
 public:
   UnstableState(std::vector<State<T>*> daughters, std::vector<Ratio*> ratios);//it always takes ownership of the pointers passed
   UnstableState(std::vector<State<T>*> daughters);
-  UnstableState(const UnstableState<T>& other);
+  UnstableState(const UnstableState<T>& other) = default;
   UnstableState(UnstableState<T>&& other) = default;
-  UnstableState<T>& operator=(const UnstableState<T>& other);
+  UnstableState<T>& operator=(const UnstableState<T>& other) = default;
   UnstableState<T>& operator=(UnstableState<T>&& other) = default;
   unsigned getDimensionOfRealisations() const;
   T getRealisation(std::mt19937& randomGenerator);//picks random ratios and returns the resulting spectrum
-  void addDaughter(State<T>* daughter);
-  void addDaughter(State<T>* daughter, Ratio* ratio);
   std::unique_ptr<State<T>> clone() const;
 
 };
 
 template <class T>
-UnstableState<T>::UnstableState(std::vector<State<T>*> daughters, std::vector<Ratio*> ratios):State<T>(daughters),ratios(ratios.begin(),ratios.end()){
+UnstableState<T>::UnstableState(std::vector<State<T>*> daughters, std::vector<Ratio*> ratios):State<T>(daughters,ratios){
   
 }
 
 template <class T>
-UnstableState<T>::UnstableState(std::vector<State<T>*> daughters):UnstableState(daughters, {}){
+UnstableState<T>::UnstableState(std::vector<State<T>*> daughters):State<T>(daughters){
   
-  for(const auto& daughter : State<T>::daughters) ratios.emplace_back(new UniformRatio);
-  
-}
-
-template <class T>
-UnstableState<T>::UnstableState(const UnstableState< T >& other):State<T>(other){
-
-  ratios.reserve(other.ratios.size());
-  for(const auto& ratio : other.ratios) this->ratios.emplace_back(ratio->clone());
-
-}
-
-template <class T>
-UnstableState<T>& UnstableState<T>::operator=(const UnstableState<T>& other){
-  
-  ratios.resize(other.ratios.size());
-  for(auto it = std::make_pair(ratios.begin(), other.ratios.begin()); it.first != ratios.end() && it.second != other.ratios.end(); ++it.first, ++it.second)
-    *it.first = std::move((*it.second)->clone());
-
 }
 
 template <class T>
 unsigned UnstableState<T>::getDimensionOfRealisations() const{
 
-  for(const auto& daughter : State<T>::daughters) return daughter->getDimensionOfRealisations();
+  for(const auto& daughter : this->daughters) return daughter->getDimensionOfRealisations();
   return 0;
   
 }
@@ -67,32 +43,17 @@ T UnstableState<T>::getRealisation(std::mt19937& randomGenerator){
 
   T realisation;//use a temporary to allow for a 'std::move'
   
-  Eigen::ArrayXd ratioValues(ratios.size());
-  for(auto itPair = std::make_pair(ratioValues.data(), ratios.begin()); itPair.first != ratioValues.data()+ratios.size() && itPair.second != ratios.end(); ++itPair.first, ++itPair.second)
+  Eigen::ArrayXd ratioValues(this->ratios.size());//need "this" for the dependent name lookup in the base class
+  for(auto itPair = std::make_pair(ratioValues.data(), this->ratios.begin()); itPair.first != ratioValues.data()+this->ratios.size() && itPair.second != this->ratios.end(); ++itPair.first, ++itPair.second)
     *itPair.first = (*itPair.second)->getRealisation(randomGenerator);
   ratioValues = ratioValues/ratioValues.sum();
 
-  for(auto itPair = std::make_pair(ratioValues.data(), State<T>::daughters.begin()); itPair.first != ratioValues.data()+ratioValues.size() && itPair.second != State<T>::daughters.end(); ++itPair.first, ++itPair.second)
+  for(auto itPair = std::make_pair(ratioValues.data(), this->daughters.begin()); itPair.first != ratioValues.data()+ratioValues.size() && itPair.second != this->daughters.end(); ++itPair.first, ++itPair.second)
     realisation += (*itPair.first) * (*itPair.second)->getRealisation(randomGenerator);
 
   return realisation;
   
 };
-
-template <class T>
-void UnstableState<T>::addDaughter(State<T>* daughter, Ratio* ratio){
-  
-  State<T>::daughters.emplace_back(daughter);
-  ratios.emplace_back(ratio);
-  
-}
-
-template <class T>
-void UnstableState<T>::addDaughter(State<T>* daughter){
-  
-  addDaughter(daughter, new UniformRatio);
-  
-}
 
 template <class T>
 std::unique_ptr< State< T > > UnstableState<T>::clone() const{
