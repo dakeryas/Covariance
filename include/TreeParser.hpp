d2a4254d81d2c1e379f7937ccb45cde1fb1e3a99
Dakeryas <mrdakeryas@gmail.com>
2015-04-05 18:23:25 +0200

    Added a normaliseData boolean to TreeParser to call normalise(data) on the data found  the datafiles of the xml tree

--- include/TreeParser.hpp
+++ include/TreeParser.hpp
@@ -12,16 +12,16 @@ template <class T>
 class TreeParser{ //class to parse xml files containing decay trees and build an unstable state from the root state of the xml file
 
   boost::filesystem::path databasePath;
-  void parse(const boost::property_tree::ptree& tree, State<T>& state);
+  void parse(const boost::property_tree::ptree& tree, State<T>& state, bool normaliseData);//parse the children of tree and append them to state
   
 public:
   TreeParser()= default;
-  std::unique_ptr<UnstableState<T>> read(std::istream& stream);
+  std::unique_ptr<UnstableState<T>> read(std::istream& stream, bool normaliseData);//read the tree defined in "stream" and normalise the data found if demanded
   
 };
 
 template <class T>
-void TreeParser<T>::parse(const boost::property_tree::ptree& tree, State<T>& state){
+void TreeParser<T>::parse(const boost::property_tree::ptree& tree, State<T>& state, bool normaliseData){
 
   for(const auto& node : tree){
     
@@ -32,12 +32,13 @@ void TreeParser<T>::parse(const boost::property_tree::ptree& tree, State<T>& sta
       
       if(ratio && error) state.addDaughter(new UnstableState<T>, new GaussianRatio(*ratio, *error));
       else state.addDaughter(new UnstableState<T>);//if we have no ratios, a default uniform ratio U(0,1) is built
-      parse(node.second, *(state.getDaughters().back()));//if the state is unstable, it means we have to parse deeper into the decay tree add append to the newly added unstable state
+      parse(node.second, *(state.getDaughters().back()), normaliseData);//if the state is unstable, it means we have to parse deeper into the decay tree add append to the newly added unstable state
       
     }
     else if(node.first == "FS"){
       
       auto spectrum = FileReader::read(databasePath/node.second.get<boost::filesystem::path>("<xmlattr>.datafile"));//retrieve the datafile field as boost::fs::path
+      if(normaliseData) normalise(spectrum);
       if(ratio && error) state.addDaughter(new FinalState<T>(spectrum), new GaussianRatio(*ratio, *error));
       state.addDaughter(new FinalState<T>(spectrum));//may have ratio and error attributes !
       
@@ -48,7 +49,7 @@ void TreeParser<T>::parse(const boost::property_tree::ptree& tree, State<T>& sta
 }
 
 template <class T>
-std::unique_ptr<UnstableState<T>> TreeParser<T>::read(std::istream& stream){
+std::unique_ptr<UnstableState<T>> TreeParser<T>::read(std::istream& stream, bool normaliseData){
   
   boost::property_tree::ptree tree;
   boost::property_tree::read_xml(stream, tree);
@@ -58,7 +59,7 @@ std::unique_ptr<UnstableState<T>> TreeParser<T>::read(std::istream& stream){
   std::cout<<"Using database: "<<databasePath<<std::endl;
   
   std::unique_ptr<UnstableState<T>> state(new UnstableState<T>);
-  parse(tree.get_child("US"), *state);//fill the state
+  parse(tree.get_child("US"), *state, normaliseData);//fill the state
   
   return state;
   
