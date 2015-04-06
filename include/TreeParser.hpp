f0ba56d058836c2e1f299910efd3bfc993d3b606
Dakeryas <mrdakeryas@gmail.com>
2015-04-06 16:38:51 +0200

    The normalise option is now passed within the xml trees and read on-fly by TreeParser

--- include/TreeParser.hpp
+++ include/TreeParser.hpp
@@ -12,16 +12,17 @@ template <class T>
 class TreeParser{ //class to parse xml files containing decay trees and build an unstable state from the root state of the xml file
 
   boost::filesystem::path databasePath;
-  void parse(const boost::property_tree::ptree& tree, State<T>& state, bool normaliseData);//parse the children of tree and append them to state
+  bool normaliseData;//normalise the data found the in databasePath
+  void parse(const boost::property_tree::ptree& tree, State<T>& state);//parse the children of tree and append them to state
   
 public:
   TreeParser()= default;
-  std::unique_ptr<UnstableState<T>> read(std::istream& stream, bool normaliseData);//read the tree defined in "stream" and normalise the data found if demanded
+  std::unique_ptr<UnstableState<T>> read(std::istream& stream);//read the tree defined in "stream" and normalise the data found if demanded
   
 };
 
 template <class T>
-void TreeParser<T>::parse(const boost::property_tree::ptree& tree, State<T>& state, bool normaliseData){
+void TreeParser<T>::parse(const boost::property_tree::ptree& tree, State<T>& state){
 
   for(const auto& node : tree){
     
@@ -32,7 +33,7 @@ void TreeParser<T>::parse(const boost::property_tree::ptree& tree, State<T>& sta
       
       if(ratio && error) state.addDaughter(new UnstableState<T>, new GaussianRatio(*ratio, *error));
       else state.addDaughter(new UnstableState<T>);//if we have no ratios, a default uniform ratio U(0,1) is built
-      parse(node.second, *(state.getDaughters().back()), normaliseData);//if the state is unstable, it means we have to parse deeper into the decay tree add append to the newly added unstable state
+      parse(node.second, *(state.getDaughters().back()));//if the state is unstable, it means we have to parse deeper into the decay tree add append to the newly added unstable state
       
     }
     else if(node.first == "FS"){
@@ -49,7 +50,7 @@ void TreeParser<T>::parse(const boost::property_tree::ptree& tree, State<T>& sta
 }
 
 template <class T>
-std::unique_ptr<UnstableState<T>> TreeParser<T>::read(std::istream& stream, bool normaliseData){
+std::unique_ptr<UnstableState<T>> TreeParser<T>::read(std::istream& stream){
   
   boost::property_tree::ptree tree;
   boost::property_tree::read_xml(stream, tree);
@@ -58,8 +59,13 @@ std::unique_ptr<UnstableState<T>> TreeParser<T>::read(std::istream& stream, bool
   databasePath = tree.get_child("database").get<boost::filesystem::path>("<xmlattr>.path");//fill the databasePath
   std::cout<<"Using database: "<<databasePath<<std::endl;
   
+  auto normaliseChild = tree.get_child_optional("normalise");
+  if(normaliseChild) normaliseData = normaliseChild->get<bool>("<xmlattr>.true");
+  else normaliseData = false;
+  std::cout<<"Normalise the data read: "<<std::boolalpha<<normaliseData<< std::endl;
+  
   std::unique_ptr<UnstableState<T>> state(new UnstableState<T>);
-  parse(tree.get_child("US"), *state, normaliseData);//fill the state
+  parse(tree.get_child("US"), *state);//fill the state
   
   std::cout<<"Read tree:\n"<<*state<<std::endl;
   
