65ade63f40163bcd591e82b5181b1967f57eff57
Dakeryas <mrdakeryas@gmail.com>
2015-05-16 17:11:42 +0200

    Added a verbose option

--- Covariance.cpp
+++ Covariance.cpp
@@ -25,26 +25,27 @@ CorrelationEstimator<US> getCorrelation(const US& unstableState1, const US& unst
   
 }
 
-void saveCovariance(const std::vector<std::string>& xmlFiles, const std::string& outFileName, double epsilon, unsigned cauchyNumber){
+void saveCovariance(const std::vector<std::string>& xmlFiles, const std::string& outFileName, double epsilon, unsigned cauchyNumber, unsigned verbose){
   
   std::vector<std::unique_ptr<std::istream>> xmlStreams;//we need pointers for polymorphism (ifstream: public istream)
   for(const auto& xmlFile : xmlFiles) xmlStreams.emplace_back(new std::ifstream(xmlFile));
   
   TreeParser<Hist> treeParser;//the  data files to parse point to "Hist" 's
   std::vector<std::unique_ptr<US>> unstableStates;
-  for(const auto& xmlStream : xmlStreams) unstableStates.emplace_back(treeParser.read(*xmlStream));//parse the xml files
+  for(const auto& xmlStream : xmlStreams) unstableStates.emplace_back(treeParser.read(*xmlStream, verbose));//parse the xml files
   
   if(unstableStates.size() == 1){
     
     auto varianceEstimator = getVariance(*unstableStates.front(), epsilon, cauchyNumber);
-    std::cout<<varianceEstimator<<std::endl;
+    if(verbose > 2) std::cout<<varianceEstimator<<std::endl;
     
-    auto mean = unstableStates.front()->getTemplateRealisation();//use a dummy realisation to get the right edges
-    mean.setBinContents(varianceEstimator.getMean());
-    mean.setErrorsFrom(varianceEstimator.getCovarianceMatrix());
     TMatrixD covarianceMatrix(varianceEstimator.getCovarianceMatrix().rows(), varianceEstimator.getCovarianceMatrix().cols(), varianceEstimator.getCovarianceMatrix().data());
     auto corr = varianceEstimator.getCorrelationMatrix();
     TMatrixD correlationMatrix(corr.rows(), corr.cols(), corr.data());
+    auto mean = unstableStates.front()->getTemplateRealisation();//use a dummy realisation to get the right edges
+    mean.setBinContents(varianceEstimator.getMean());
+    mean.setErrorsFrom(varianceEstimator.getCovarianceMatrix());
+    
     TFile outfile(outFileName.c_str(), "recreate");
     mean.Write();
     covarianceMatrix.Write("Cov");
@@ -54,23 +55,23 @@ void saveCovariance(const std::vector<std::string>& xmlFiles, const std::string&
   else if(unstableStates.size() == 2){
     
     auto correlationEstimator = getCorrelation(*unstableStates.front(), *unstableStates.back(), epsilon, cauchyNumber);
-    std::cout<<correlationEstimator<<std::endl;
+    if(verbose > 2) std::cout<<correlationEstimator<<std::endl;
     
-    auto mean1 = unstableStates.front()->getTemplateRealisation();//use a dummy realisation to get the right edges
-    mean1.setBinContents(correlationEstimator.getMean1());
-    mean1.setErrorsFrom(correlationEstimator.getCovarianceMatrix());
     auto var1 = correlationEstimator.getVarianceEstimator1().getCovarianceMatrix();
     TMatrixD covarianceMatrix1(var1.rows(), var1.cols(), var1.data());
     auto corr1 = correlationEstimator.getVarianceEstimator1().getCorrelationMatrix();
     TMatrixD correlationMatrix1(corr1.rows(), corr1.cols(), corr1.data());
+    auto mean1 = unstableStates.front()->getTemplateRealisation();//use a dummy realisation to get the right edges
+    mean1.setBinContents(correlationEstimator.getMean1());
+    mean1.setErrorsFrom(var1);
 
-    auto mean2 = unstableStates.back()->getTemplateRealisation();//use a dummy realisation to get the right edges
-    mean2.setBinContents(correlationEstimator.getMean2());
-    mean2.setErrorsFrom(correlationEstimator.getCovarianceMatrix());
     auto var2 = correlationEstimator.getVarianceEstimator2().getCovarianceMatrix();
     TMatrixD covarianceMatrix2(var2.rows(), var2.cols(), var2.data());
     auto corr2 = correlationEstimator.getVarianceEstimator2().getCorrelationMatrix();
     TMatrixD correlationMatrix2(corr2.rows(), corr2.cols(), corr2.data());
+    auto mean2 = unstableStates.back()->getTemplateRealisation();//use a dummy realisation to get the right edges
+    mean2.setBinContents(correlationEstimator.getMean2());
+    mean2.setErrorsFrom(var2);
 
     TMatrixD covarianceMatrix12(correlationEstimator.getCovarianceMatrix().rows(), correlationEstimator.getCovarianceMatrix().cols(), correlationEstimator.getCovarianceMatrix().transpose().data());
     auto corr12 = correlationEstimator.getCorrelationMatrix().transpose();
@@ -98,7 +99,8 @@ int main (int argc, char* argv[]){
   ("tree,t", bpo::value<std::vector<std::string>>(), "Decay trees to process in the form of XML files (1 or 2 files)")
   ("output,o", bpo::value<std::string>(), "Output file where to save the covariance matrices")
   ("accuracy,a", bpo::value<double>()->default_value(1e-4) ,"Demanded relative accuracy for the covariance matrix")
-  ("consecutive,c", bpo::value<unsigned>()->default_value(3),"Number of close consecutive matrices demanded");
+  ("consecutive,c", bpo::value<unsigned>()->default_value(3),"Number of close consecutive matrices demanded")
+  ("verbose,v", bpo::value<unsigned>()->default_value(2),"Display parsing at 1, complete parsing at 2, and display all results at 3");
 
   bpo::positional_options_description positionalOptions;//to use arguments without "--"
   positionalOptions.add("tree", -1);
@@ -125,7 +127,7 @@ int main (int argc, char* argv[]){
 	    
 	}
 	
-	if(regularFiles) saveCovariance(xmlFiles, arguments["output"].as<std::string>(), arguments["accuracy"].as<double>(), arguments["consecutive"].as<unsigned>());
+	if(regularFiles) saveCovariance(xmlFiles, arguments["output"].as<std::string>(), arguments["accuracy"].as<double>(), arguments["consecutive"].as<unsigned>(), arguments["verbose"].as<unsigned>());
 	  
       }
       
